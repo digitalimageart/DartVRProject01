54ba0168989d14757d37d939fba2a41a5613bbca
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-21 09:23:33 +0900

    initial_commit

---
+++ Assets/01_Scripts/Instanciator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciator : MonoBehaviour {
+
+    public GameObject gameObject;
+    
+
+    private void Start()
+    {
+        StartCoroutine("Maker");
+    }
+
+    private IEnumerator Maker()
+    {
+        while (true)
+        {
+            Instantiate(gameObject.transform,transform.position,Quaternion.identity);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
---
+++ Assets/01_Scripts/Remover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Remove");
+	}
+
+    IEnumerator Remove()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        Destroy(this.gameObject);
+    }
+	
+}
