1b1d8abcad2a2417858cd3cfcffb0868e26b314e
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-21 13:58:14 +0900

    addDataManager

---
+++ Assets/01_Scripts/DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour {
+
+    // SingleTon
+    private static DataManager _Instance;
+
+    private DataManager() { }
+    public static DataManager Instance {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = new DataManager();
+            }
+            return _Instance;
+        }
+    }
+
+    //Read Only Score
+    private int _score;
+    public int score { get { return _score; } }
+    //StopWatch
+    public System.Diagnostics.Stopwatch stopwatch
+    {
+        get { return new System.Diagnostics.Stopwatch(); }
+    }
+
+
+}
