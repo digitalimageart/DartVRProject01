eacb67c364c692afbabe9532a237fee2ea8415ec
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-22 10:29:56 +0900

    addTarget

---
+++ Assets/01_Scripts/Object/ThrowRelated/ThrownCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownCube : ThrownObject {
+
+    //부딪혔을 때 상대 오브젝트를 받아 HeadMount이면 영향을 주자
+    public override void hit()
+    {
+        
+    }
+
+}
