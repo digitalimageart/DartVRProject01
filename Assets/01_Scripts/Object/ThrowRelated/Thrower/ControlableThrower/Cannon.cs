b7ceb8b22eb5e920c4c2d38cbdd4366e6f4a3c7a
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-22 16:08:39 +0900

    addThrowerType

---
+++ Assets/01_Scripts/Object/ThrowRelated/Thrower/ControlableThrower/Cannon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : ControllableThrower
+{
+
+    protected override IEnumerator PressContinuously()
+    {
+        while (true)
+        {
+            //크기값을 받아 크기를 키워주기
+            //무게값을 받아 무게를 키워주기
+            //이펙트 넣어주기
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public override void ThrowObject()
+    {
+        //
+        madeObject.GetComponent<Rigidbody>().AddForce(transform.forward * _throwerPower /* 곱하기 추가된 무게를 더 해 주어야 한다.*/);
+    }
+
+    
+}
