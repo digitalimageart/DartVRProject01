1f10dd2631385b7d536658b8e388573c415ac710
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-22 09:43:42 +0900

    addThrowAction

---
+++ Assets/01_Scripts/Object/ThrownCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownCube : MonoBehaviour,ThrownObject {
+
+    public void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * 10000);
+
+        StartCoroutine("Death");
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
+    }
+
+    //부딪혔을 때 상대 오브젝트를 받아 HeadMount이면 영향을 주자
+    public void hit(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
