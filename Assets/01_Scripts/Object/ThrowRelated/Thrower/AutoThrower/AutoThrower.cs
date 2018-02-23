f67ecb73939d2a583e0cdd968ee9fd835eddcacf
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-02-23 12:02:20 +0900

    reStart

---
+++ Assets/01_Scripts/Object/ThrowRelated/Thrower/AutoThrower/AutoThrower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoThrower : MonoBehaviour,Thrower {
+
+    [SerializeField]
+    private GameObject _thrownObject;
+    [SerializeField]
+    private float _throwerPower;
+
+    private GameObject madeObejct;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("ThrowContinuously");
+	}
+
+    IEnumerator ThrowContinuously()
+    {
+        while (true)
+        {
+            MakeThrownObject();
+            ThrowObject();
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public void ThrowObject()
+    {
+        madeObejct.GetComponent<Rigidbody>().AddForce(transform.forward * _throwerPower);
+    }
+
+    public void MakeThrownObject()
+    {
+        madeObejct = Instantiate(_thrownObject, transform.position, Quaternion.identity);
+    }   
+}
---
+++ Assets/01_Scripts/Object/ThrowRelated/Thrower/ControlableThrower/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : ControllableThrower
+{
+
+    protected override IEnumerator PressContinuously()
+    {
+		MakeThrownObject ();
+		madeObject.GetComponent<Rigidbody> ().useGravity = false;
+        while (true)
+        {
+            if (madeObject != null)
+            {
+                madeObject.transform.localScale += new Vector3(10, 10, 10)*Time.deltaTime;
+                //크기값을 받아 크기를 키워주기
+                //무게값을 받아 무게를 키워주기
+                //이펙트 넣어주기
+            }
+            yield return new WaitForSeconds(0.0001f);
+        }
+    }
+
+    public override void ThrowObject()
+    {   
+        if (madeObject != null)
+        {
+            madeObject.GetComponent<Rigidbody>().useGravity = true;
+            madeObject.GetComponent<Rigidbody>().AddForce(transform.forward * _throwerPower /* 곱하기 추가된 무게를 더 해 주어야 한다.*/);
+        }
+    }
+
+    
+}
