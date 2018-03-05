d25ecd46a7101f3d69a96a6b4842c6eabd5e8981
suy427 <sondahum@gmail.com>
2018-03-05 22:12:12 +0900

    addSingleton

--- Assets/01_Scripts/Object/ThrowRelated/Thrown/ThrownMonster.cs
+++ Assets/01_Scripts/Object/ThrowRelated/Thrown/ThrownMonster.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ThrownMonster : Bullet
 {
+    public override void Effect()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public override void Hit(GameObject obj)
     {
     }
---
+++ Assets/01_Scripts/Util/SingleTon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SingleTon<T> : MonoBehaviour where T : MonoBehaviour {
+
+
+    // SingleTon
+    private static T _intstance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_intstance == null)
+            {
+                _intstance = FindObjectOfType(typeof(T)) as T;
+                if (_intstance == null)
+                    Debug.LogError("There needs to be one active MyClass script on a GameObject in your scene.");
+            }
+
+            return _intstance;
+        }
+    }
+}
